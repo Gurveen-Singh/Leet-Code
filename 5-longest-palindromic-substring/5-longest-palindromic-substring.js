02c4f390ae34b672c75815e1eeb5380a8522d64f
Gurveen Singh <55324834+Gurveen-Singh@users.noreply.github.com>
2022-02-28 22:43:59 +0530

    Time: 246 ms (45.31%), Space: 49.5 MB (20.91%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,24 +3,13 @@
  * @return {string}
  */
 
-var longestPalindrome = function(s) {
-    let max = '';
-    for (let i = 0; i < s.length; i++) {
-        for (const j of [0, 1]) {
-            let left = i;
-            let right = i + j;
-            while (left >= 0 && s[left] === s[right]) {
-                left--;
-                right++;
-            }
-
-            if ((right - left - 1) > max.length) {
-              max = s.substring(left + 1, right);
-            }   
-        }
-        
-        // No better move exists
-        if (Math.ceil(max.length / 2) >= s.length - i) break;
-    }
-    return max;
-};;
+function longestPalindrome(s) {
+  let ll = 0, rr = 0;
+  
+  for (let i = 0; i < s.length; i++)
+    for (let j of [i, i+1])
+      for (l = i, r = j; s[l] && s[l] === s[r]; l--, r++)
+        [ll, rr] = (r-l+1) > (rr-ll+1) ? [l, r] : [ll, rr];
+  
+  return s.substring(ll, rr+1);
+}
