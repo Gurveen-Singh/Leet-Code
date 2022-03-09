471618c7a2a17b90a6806291a64e38431cc74b65
Gurveen Singh <55324834+Gurveen-Singh@users.noreply.github.com>
2022-03-09 21:42:12 +0530

    Time: 215 ms (12.18%), Space: 44.2 MB (59.90%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = nums => {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) nums.splice(i--, 1)
+  }
+
+  return nums.length
+}
